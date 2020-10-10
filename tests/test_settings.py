63abd11d2d315ef428b461d80a2ff5ef2b79c0a7
Ron Heimann <ron.heimann@gmail.com>
2020-10-10 16:18:38 +0300

    Test extract_email_from_firebase_user method

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,11 @@
+"""Project settings for testing"""
+
+from pathlib import Path
+
 SECRET_KEY = 'fake-key'
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -6,3 +13,10 @@ INSTALLED_APPS = [
     'drf_firebase_token_auth',
     'tests',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
